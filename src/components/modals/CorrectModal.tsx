663518c3e47005315b6541883ce6980b798e1547
Rostyslav Kravchenko <rostyslav.kravchenko@tui.co.uk>
2022-02-15 13:46:44 +0200

    step

--- src/components/modals/CorrectModal.tsx
+++ src/components/modals/CorrectModal.tsx
@@ -1,5 +1,7 @@
 import { WORDS } from '../../constants/wordlist'
 import { BaseModal } from './BaseModal'
+import { tomorrow } from '../../lib/words'
+import Countdown from 'react-countdown'
 
 type Props = {
   isOpen: boolean
@@ -8,14 +10,19 @@ type Props = {
 }
 
 export const CorrectModal = ({ isOpen, handleClose, solution }: Props) => {
-  console.log(solution)
   return (
     <BaseModal title="" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-xs text-left mb-2 text-gray-700 dark:text-gray-300">
         Вітаємо!
         Більше вгаданих слів — краща підготовка!
 
-        Нове слово через 12:09:34Не хочете чекати на нові знання? Завітайте на ….. , щоб дізнатись більше.
+        Нове слово
+        <Countdown
+          className="text-lg font-medium text-gray-900 dark:text-gray-100"
+          date={tomorrow}
+          daysInHours={true}
+        />
+        Не хочете чекати на нові знання? Завітайте на ….. , щоб дізнатись більше.
       </p>
       <p className="text-xs text-left mb-2 text-gray-700 dark:text-gray-300">
         {
